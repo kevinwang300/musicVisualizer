28ba77613c9a1fe4f278d504fff7613e9c51d97b
Kevin Wang <kevinwang300@gmail.com>
2017-10-08 03:50:14 -0700

    set song and tempo

--- MusVisPack/MusicVisualizer.java
+++ MusVisPack/MusicVisualizer.java
@@ -21,10 +21,17 @@ public class MusicVisualizer implements LineListener {
      */
     boolean playCompleted;
     Turtle visualizer = new Turtle(0.5, 0.5, 0);
+    double songBeat;
+
+    public MusicVisualizer(double beat) {
+        songBeat = beat;
+    }
+
     /**
      * Play a given audio file.
      * @param audioFilePath Path of the audio file.
      */
+
     void play(String audioFilePath) {
         File audioFile = new File(audioFilePath);
         int counter = 1;
@@ -49,25 +56,35 @@ public class MusicVisualizer implements LineListener {
             StdDraw.clear(StdDraw.BLACK);
             visualizer.setPenColor(Color.getHSBColor((float) Math.random(), .8f, .8f));
             Color newColor = Color.getHSBColor((float) Math.random(), .8f, .8f);
+            Color backgroundColor = Color.getHSBColor((float) Math.random(), .8f, .8f);
             audioClip.start();
+            double angle = 90.25;
             while (!playCompleted) {
                 // wait for the playback completes
                 try {
                     Thread.sleep(1000 / bpm);
-                    int loopLength = 150;
-                    int clearCount = 16 * loopLength;
+                    double loopLength = 150;
+                    double clearCount = 8 * loopLength;
+                    double circleAngle = 360;
+                    if (newColor == backgroundColor) {
+                        newColor = Color.getHSBColor((float) Math.random(), .8f, .8f);
+                    }
                     visualizer.goForward((counter % loopLength) * 0.01);
-                    visualizer.turnLeft(90.25);
+                    visualizer.turnLeft(angle);
                     counter++;
                     if ((counter % loopLength) == 0) {
                         visualizer.x = 0.5;
                         visualizer.y = 0.5;
-                        visualizer.angle = 5 * (counter / loopLength);
+                        //visualizer.angle = (circleAngle / 8) * (counter / loopLength);
+                        visualizer.angle = (angle * loopLength) % circleAngle + (circleAngle / 8) * (counter % 8);
+                        //System.out.println(visualizer.angle);
                         newColor = Color.getHSBColor((float) Math.random(), .8f, .8f);
                         visualizer.setPenColor(newColor);
                     }
                     if ((counter % clearCount) == 0) {
-                        StdDraw.clear(newColor);
+                        backgroundColor = Color.getHSBColor((float) Math.random(), .8f, .8f);
+                        StdDraw.clear(backgroundColor);
+                        angle = 90.25 + Math.random() * 199.75;
                     }
 //                    if ((counter % clearCount) == loopLength) {
 //                        visualizer.x = 0.25;
@@ -129,9 +146,10 @@ public class MusicVisualizer implements LineListener {
     public static void main(String[] args) {
         System.out.println("Enter an audio file path:");
         Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
-        String audioFilePath = "/Users/dangdang98/calHacks2017/musicVisualizer/MusVisPack/07_The_Less_I_Know_The_Better.wav";
-        MusicVisualizer player = new MusicVisualizer();
-        player.play(username);
+        String soundFile = scanner.nextLine();
+        double beat = 72;
+        String audioFilePath = "/Users/dangdang98/calHacks2017/musicVisualizer/MusVisPack/1-06 Redbone.wav";
+        MusicVisualizer player = new MusicVisualizer(beat);
+        player.play(soundFile);
     }
 }
